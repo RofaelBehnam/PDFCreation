71c0e5a33be0b27259cfb1a55f2d5c769695187a
Rofael <rofaelemil@gmail.com>
2025-05-26 10:27:05 +0300

    create PDF

--- src/main/java/org/example/HTMLAspose.java
+++ src/main/java/org/example/HTMLAspose.java
@@ -12,14 +12,14 @@ public class HTMLAspose {
         // Create a HTML LoadOptions
         HtmlLoadOptions options = new HtmlLoadOptions();
 
-// Set Print or Screen mode
+        // Set Print or Screen mode
         options.setHtmlMediaType(HtmlMediaType.Print);
 
-// Initialize document object
+        // Initialize document object
         String htmlFileName = Paths.get(DATA_DIR.toString(), "html.html").toString();
         Document document = new Document(htmlFileName, options);
 
-// Save output PDF document
+        // Save output PDF document
         document.save(Paths.get(DATA_DIR.toString(), "html.pdf").toString());
         document.close();
     }
